0d1316fe3fc9f845163ee96dd2805afe87fd9bdd
Syed Mona Javed <monajaved93@gmail.com>
2020-11-06 06:32:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,4 +2,23 @@ class TicTacToe
   def initialize
     @board = board || Array.new(9, " ")
   end
+
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],  # Bottom row
+  [0,3,6],  # Left col
+  [1,4,7],  # Middle col
+  [2,5,8],  # Right col
+  [0,4,8],  # Diagnol 1
+  [2,4,6]  # Diagnol 2
+]
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  puts "-----------"
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  puts "-----------"
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+end
+
 end
